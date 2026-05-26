f7c812635daa7212f7004443690fba43975e9a60
Anna <3banana1cola@gmail.com>
2026-05-26 19:28:36 +0300

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Калькулятор");
+        System.out.println("5 + 3 = " + add(5, 3));
+        System.out.println("10 - 4 = " + subtract(10, 4));
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
+
+(Добавила простой калькулятор)
